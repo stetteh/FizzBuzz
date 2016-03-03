e0301eb0c1d253b676b4eb05ec6dc59d80ec0e58
Seth Tetteh-Quaye <stettehq@gmail.com>
2016-03-03 10:10:54 -0600

    intial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Fizz Buzz");
+
+            //loop for 100 numbers
+            for(int i = 0; i <= 100; i++)
+            {
+                Console.Write(i); //print to screen the numbers
+
+                //check if its divisible by 3
+                if(i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                ////check if its divisible by 5
+                if(i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                Console.WriteLine(); //print onto next line
+            }
+            Console.ReadLine();
+        }
+    }
+}
